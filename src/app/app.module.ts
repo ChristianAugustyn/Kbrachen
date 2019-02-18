61378696623dbabc725a444398a515a2c2647e1a
Christian <chrisaug@rogers.com>
2019-02-18 15:10:14 -0500

    test for hosting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title} from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -23,7 +23,9 @@ import {MatGridListModule} from '@angular/material/grid-list';
     MatButtonModule,
     MatGridListModule
   ],
-  providers: [],
+  providers: [
+    Title
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
